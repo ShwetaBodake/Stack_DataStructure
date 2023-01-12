ae3783abf7ee7db53d6407ca7e494a23494c2d22
ShwetaBodake <shwetabodake8108@gmail.com>
2023-01-12 22:48:03 +0530

    Stack is generated with disply method

---
+++ LinkedListStack.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace Stack_DataStructure
+{
+	public class LinkedListStack
+	{
+		private Node top;
+		public LinkedListStack()
+		{
+			this.top = null;
+		}
+
+		public void push(int value)
+		{
+			Node node = new Node(value);
+			if(this.top==null)
+			{
+				node.next = null;
+			}
+			else
+			{
+				node.next = this.top;
+
+			}
+			this.top = node;
+			Console.WriteLine("{0} pushed to stack " + value);
+		}
+
+
+		public void display()
+		{
+			Node temp = this.top;
+			while(temp!= null)
+			{
+				Console.Write(temp.data + " ");
+				temp = temp.next;
+			}
+		}
+	}
+}
+
