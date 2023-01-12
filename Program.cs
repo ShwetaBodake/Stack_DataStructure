f3c9dc49b917d025603d08fa0d8c4790f0ad322c
ShwetaBodake <shwetabodake8108@gmail.com>
2023-01-12 22:46:36 +0530

    Stack is created using linked list

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Stack_DataStructure
+{
+ public class Program
+    {
+        public static void Main(string[] args)
+        {
+            LinkedListStack stack = new LinkedListStack();
+            stack.push(70);
+            stack.push(30);
+            stack.push(56);
+            stack.display();
+
+        }
+    }
+}
