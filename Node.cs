410656d4d5633986c5ca8cb9574412d09ec22d09
ShwetaBodake <shwetabodake8108@gmail.com>
2023-01-12 22:45:26 +0530

    node class is generated

---
+++ Node.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Stack_DataStructure
+{
+	public class Node
+	{
+        public int data;
+        public Node next;
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+}
+
