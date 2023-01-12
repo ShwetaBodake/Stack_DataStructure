32d72e38fdfc49125f3bf03636e78aea7de0d575
ShwetaBodake <shwetabodake8108@gmail.com>
2023-01-12 23:23:00 +0530

    Stack Peak and Pop method is created

--- LinkedListStack.cs
+++ LinkedListStack.cs
@@ -35,6 +35,27 @@ namespace Stack_DataStructure
 				temp = temp.next;
 			}
 		}
+
+		public void peek()
+		{
+			if(this.top==null)
+			{
+				Console.WriteLine("Stack is empty");
+				return;
+			}
+			Console.WriteLine("{ 0} is in the top of the stack " + this.top.data);
+		}
+
+		public void pop()
+		{
+			if(this.top==null)
+			{
+				Console.WriteLine("Stack is empty,Deletion is not possible");
+				return;
+			}
+			Console.WriteLine("Value popped is {0}", this.top.data);
+			this.top = this.top.next;
+		}
 	}
 }
 
